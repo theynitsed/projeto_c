5a32ecaa1e3d47dc36fbda59d01d5708385a0cc2
destiny <102867882+theynitsed@users.noreply.github.com>
2022-04-11 12:22:02 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,240 @@
+import './App.css';
+import social1 from './img/social/discord.png';
+import social2 from './img/social/github.png';
+import social3 from './img/social/instagram.png';
+import social4 from './img/social/twitter.png';
+import image1 from "./img/comics/demonicemperor.png";
+import image2 from "./img/comics/overgeared.jpeg";
+import image3 from "./img/comics/RFP.png";
+import image4 from "./img/comics/theheroreturnscover.png";
+
+function App() {
+  return (
+    <div className="App">
+
+
+<div className="container">
+
+  <div className="header">
+ 
+    <div className="h-logo">
+      <h3>Projeto C</h3>
+    </div>
+    <div className="h-navbar">
+            <nav>
+                <ul>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="comics.html">Comics</a></li>
+                  <li><a href="novel.html">Novel</a></li>
+                  <li><a href="novel.html">Weblist</a></li>
+                </ul>
+            </nav>
+    </div>
+         
+            
+          
+    <div className="h-navbar-sub"></div>
+  
+  </div>
+  <div className="footer"></div>
+  <div className="footer-social">
+    <div className="footer-social-container">
+      <a href="/" className="imgcontainer"><img src={social1} alt="discord" /></a>
+      <a href="/" className="imgcontainer"><img src={social2} alt="github" /></a>
+      <a href="/" className="imgcontainer"><img src={social3} alt="instagram" /></a>
+      <a href="/" className="imgcontainer"><img src={social4} alt="twitter" /></a>
+    </div>
+  </div>
+  <div className="footer-desc">
+    <div className="footer-desc-container">
+      <h5>@2022   koyand    Projeto C</h5>
+    </div>
+  </div>
+  <div className="main"></div>
+  <div className="main-h1-x1">
+    <h1 class="heading" >our <span>Comics</span></h1>
+  </div>
+
+  <div className="main-h1-x3">
+    <h1 class="heading" >our <span>Weblist</span></h1>
+  </div>
+  <div className="main-container-x1">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div>
+
+
+
+    <div className="main-h1-x1-c2">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image3}alt="" />
+      <h5>The Hero Returns</h5>
+    </div>
+
+    </div>
+    <div className="main-h1-x1-c3">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image2}alt="" />
+      <h5>Overgeared</h5>
+    </div>
+    </div>
+    <div className="main-h1-x1-c4">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image4}alt="" />
+      <h5>RFP</h5>
+    </div>
+    </div>
+    <div className="main-h1-x1-c5">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div>
+    </div>
+    <div className="main-h1-x1-c6">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div>
+    </div>
+    <div className="main-h1-x1-c7">
+    <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div>
+    </div>
+    <div className="main-h1-x1-c8"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c9"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c10"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c11"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c12"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c13"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c14"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c15"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c16"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c17"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c18"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c19"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+    <div className="main-h1-x1-c20"> <div className="main-h1-x1-c1">
+      <a href="/">MANHUA</a>
+      <h2>CH 1029</h2>
+      <img src={image1}alt="" />
+      <h5>Demonic Emperor</h5>
+    </div></div>
+  </div>
+  <div className="main-container-x3">
+    <div className="main-container-x3-text1">
+      
+    </div>
+    <div className="main-container-x3-text2">
+      <a href="https://mangalivre.net" >Mangalivre</a>
+    </div>
+    <div className="main-container-x3-text3">
+      <a href="https://neoxscans.net">Neoxscans</a>
+    </div>
+    <div className="main-container-x3-text4">
+      <a href="https://manhuazone.com" >Manhuazone</a>    
+    </div>
+    <div className="main-container-x3-text5">
+      <a href="https://www.asurascans.com" >Asurascans</a>
+    </div>
+    <div className="main-container-x3-text6">
+      <a href="https://reaperscans.com" >Reaperscans</a>
+    </div>
+    <div className="main-container-x3-text7">
+      <a href="https://scansraw.com" >Scansraw</a>
+    </div>
+    <div className="main-container-x3-text8">
+      <a href="https://mangakakalot.com" >Mangakakalot</a>
+    </div>
+    <div className="main-container-x3-text9">
+      <a href="https://manganato.com" >Manganato</a>
+    </div>
+    <div className="main-container-x3-text10"></div>
+    <div className="main-container-x3-text11"></div>
+    <div className="main-container-x3-text12"></div>
+  </div>
+</div>
+
+
+
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,24 @@
+import image1 from "./img/comics/demonicemperor.png";
+import image2 from "./img/comics/RFP.png";
+import image3 from "./img/comics/theheroreturnscover.png";
+import image4 from "./img/comics/RFP.png";
+import image5 from "./img/comics/theheroreturnscover.png";
+import image6 from "./img/comics/RFP.png";
+import image7 from "./img/comics/theheroreturnscover.png";
+import image8 from "./img/comics/demonicemperor.png";
+import image9 from "./img/comics/RFP.png";
+import image10 from "./img/comics/theheroreturnscover.png";
+import image11 from "./img/comics/RFP.png";
+import image12 from "./img/comics/theheroreturnscover.png";
+import image13 from "./img/comics/RFP.png";
+import image14 from "./img/comics/theheroreturnscover.png";
+import image15 from "./img/comics/demonicemperor.png";
+import image16 from "./img/comics/RFP.png";
+import image17 from "./img/comics/theheroreturnscover.png";
+import image18 from "./img/comics/RFP.png";
+import image19 from "./img/comics/theheroreturnscover.png";
+import image20 from "./img/comics/RFP.png";
+import image21 from "./img/comics/theheroreturnscover.png";
+
+export default [image1,image2,image3,image4,image5,image6,image7
+,image8,image9,image10,image11,image12,image13,image14,image15,image16,image17,image18,image19,image20,image21];
